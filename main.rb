99c3c651ad2d5079d50468a9a653b249b5308f2c
Pandop <s.kel@student.qut.edu.au>
2018-09-06 00:18:43 +1000

    Commented out dummy user data

--- main.rb
+++ main.rb
@@ -35,15 +35,15 @@ app_guide.run_medirecord!
 
 
 
-patients = [
-    {patient: [{username:"John",password:"123John",dob:"21/02/1990",age:"25",date:"19/02/2018",illness:"Flue",treatment: "Panadol Rapid" }]}, 
-    {patient: [{username:"Sara",password:"123Sara",dob:"01/02/1995",age:"28",date:"19/06/2018",illness:"",treatment: "" }]},
-    {patient: [{username:"Does",password:"123Does",dob:"20/05/1988",age:"30",date:"19/02/2018",illness:"",treatment: "" }]}
-    ]
-doctors  = 
-[   {doctor: "christopher",password: "111christ", age: "45", specialty: "GP"}, 
-    {doctor: "randolphoe", password: "111rando" , age: "55", specialty: "Surgeon"}
-]
+# patients = [
+#     {patient: [{username:"John",password:"123John",dob:"21/02/1990",age:"25",date:"19/02/2018",illness:"Flue",treatment: "Panadol Rapid" }]}, 
+#     {patient: [{username:"Sara",password:"123Sara",dob:"01/02/1995",age:"28",date:"19/06/2018",illness:"",treatment: "" }]},
+#     {patient: [{username:"Does",password:"123Does",dob:"20/05/1988",age:"30",date:"19/02/2018",illness:"",treatment: "" }]}
+#     ]
+# doctors  = 
+# [   {doctor: "christopher",password: "111christ", age: "45", specialty: "GP"}, 
+#     {doctor: "randolphoe", password: "111rando" , age: "55", specialty: "Surgeon"}
+# ]
 
 # secur_data = SecureData.new
 
